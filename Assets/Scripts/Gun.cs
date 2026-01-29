4ff8f599de5981fcca3f5e638f66b52e675a44b5
Samuel Lallemand <samuel.lallemand@eleves.ec-nantes.fr>
2026-01-29 11:08:51 +0100

    Spawn laser projectile from gun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float cooldown = 30;
+    public GameObject laserPrefab;
+
+    private Transform shootingPoint;
+    private InputDevice hand;
     void Start()
     {
-        
+        hand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        shootingPoint = Generic.FindChildWithTag(gameObject, "Marker")?.transform;
+        if (shootingPoint == null) Debug.LogError("No child of Gun with tag 'Marker' to shoot from");
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        // Avoid odd cooldown after swtiching while not allowing a strong quick swtiching reload exploit
+        ticking = cooldown / 3; 
+    }
+
+    private float ticking = 0;
     void Update()
     {
-        Debug.Log("Gun equipped");
+        ticking--;
+        // If on cooldown nothing to do
+        if (ticking > 0) { return; }
+
+        // Get "Grip" press to shoot gun
+        bool triggerPressed = false;
+        hand.TryGetFeatureValue(CommonUsages.triggerButton, out triggerPressed);
+        if (triggerPressed)
+        {
+            ticking = cooldown + 1;
+            GameObject laser = Instantiate(laserPrefab);
+            laser.transform.position = shootingPoint.position;
+            laser.transform.localRotation = shootingPoint.rotation;
+            
+        }
     }
 }

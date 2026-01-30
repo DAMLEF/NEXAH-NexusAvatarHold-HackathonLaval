fa5684c069d3841b3d074e928822f801b1161d8e
Samuel Lallemand <samuel.lallemand@eleves.ec-nantes.fr>
2026-01-30 13:19:16 +0100

    shop

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,17 +4,26 @@ using UnityEngine.XR;
 
 public class Gun : MonoBehaviour
 {
-    public float cooldown = 30;
+    public float cooldownBase = 30;
+    private float cooldown;
     public GameObject laserPrefab;
     public Transform laserStorage; // Hierachy management
-
+    public int gunDamage = 1;
     private Transform shootingPoint;
     private InputDevice hand;
     void Start()
     {
+        cooldown = cooldownBase;
         hand = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
         shootingPoint = Generic.FindChildWithTag(gameObject, "Marker")?.transform;
         if (shootingPoint == null) Debug.LogError("No child of Gun with tag 'Marker' to shoot from");
+    }
+
+    private void Upgrade()
+    {
+        cooldown = 0.98f * cooldown;
+        gunDamage += 1;
+        LaserBounce.allowedBounces += 1;
     }
 
     void OnEnable()
@@ -39,6 +48,8 @@ public class Gun : MonoBehaviour
 
             GameObject laser = Instantiate(laserPrefab);
             if (laserStorage) laser.transform.SetParent(laserStorage);
+            LaserBounce laserScript = laser.GetComponent<LaserBounce>();
+            if (laserScript != null) { laserScript.hitDamage = gunDamage; }
             laser.transform.position = shootingPoint.position;
             laser.transform.localRotation = shootingPoint.rotation;
         }
